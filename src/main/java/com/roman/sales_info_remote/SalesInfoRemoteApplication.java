597a7900f9ddec3ead56f9250484c431bf76ac09
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-19 15:57:54 +0200

    Initial commit

---
+++ src/main/java/com/roman/sales_info_remote/SalesInfoRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.roman.sales_info_remote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesInfoRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesInfoRemoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/sales_info_remote/SalesInfoRemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.sales_info_remote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesInfoRemoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
