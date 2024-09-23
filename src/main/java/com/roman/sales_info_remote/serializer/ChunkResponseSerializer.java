9ad422c19cf416a1d9e5720e670bba833e7e1c1f
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-23 09:53:09 +0200

    deserializer

--- src/main/java/com/roman/sales_info_remote/serializer/ChunkResponseSerializer.java
+++ src/main/java/com/roman/sales_info_remote/serializer/ChunkResponseSerializer.java
@@ -1,15 +1,16 @@
 package com.roman.sales_info_remote.serializer;
 
-import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.springframework.batch.integration.chunk.ChunkResponse;
 import org.springframework.util.SerializationUtils;
 
-public class ChunkResponseSerializer implements Serializer<ChunkResponse> {
-    @Override
-    public byte[] serialize(String s, ChunkResponse chunkResponse) {
-        if(chunkResponse == null)
-            return new byte[0];
+public class ChunkResponseSerializer implements Deserializer<ChunkResponse> {
 
-        return SerializationUtils.serialize(chunkResponse);
+    @Override
+    public ChunkResponse deserialize(String s, byte[] bytes) {
+        if(bytes == null){
+            return null;
+        }
+        return (ChunkResponse) SerializationUtils.deserialize(bytes);
     }
 }
